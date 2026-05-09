637fd4f8e4dfeb21abbb33b3c8b61cce7d00c62f
angelicamateo041219-commits <angelicamateo041219@gmail.com>
2026-05-09 16:29:58 +0800

    Update antichangeimage.js

--- modules/commands/antichangeimage.js
+++ modules/commands/antichangeimage.js
@@ -1,4 +1,15 @@
-module.exports.config = { name: "antichangeimage" };
+const { getData, setData } = require("../../database.js");
+
+module.exports.config = {
+    name: "antichangeimage",
+    version: "2.0.0",
+    hasPermssion: 0,
+    credits: "ChatGPT",
+    description: "Toggle anti group image change",
+    commandCategory: "group",
+    usages: "/antichangeimage on/off",
+    cooldowns: 5
+};
 
 module.exports.run = async ({ api, event, args }) => {
     const { threadID, senderID } = event;
@@ -8,12 +19,12 @@ module.exports.run = async ({ api, event, args }) => {
     let data = await getData("antiSystem") || {};
     if (!data.boximage) data.boximage = {};
 
-    let status = "";
+    let status = "❌";
 
-    if (args[0] == "on") {
+    if (args[0] === "on") {
         data.boximage[threadID] = true;
         status = "ON ✅";
-    } else {
+    } else if (args[0] === "off") {
         delete data.boximage[threadID];
         status = "OFF ❌";
     }
