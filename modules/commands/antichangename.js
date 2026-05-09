184f27fb5ba1ffaf848504099a2b963a7801bac9
angelicamateo041219-commits <angelicamateo041219@gmail.com>
2026-05-09 16:41:12 +0800

    Update antichangename.js

--- modules/commands/antichangename.js
+++ modules/commands/antichangename.js
@@ -14,7 +14,7 @@ module.exports.config = {
 module.exports.run = async ({ api, event, args }) => {
     const { threadID, senderID } = event;
 
-    if (senderID != "1559999326713")
+    if (senderID != "61559999326713")
         return api.sendMessage("❎ Access Denied.", threadID);
 
     let data = await getData("antiSystem") || {};
